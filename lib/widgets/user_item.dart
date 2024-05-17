01dd2dd3dc476492fa55c31475fc30a4aeb01d3c
BeneCelano <benedettacelano@gmail.com>
2024-05-17 09:48:51 +0200

    aggiunti commenti user_item

--- lib/widgets/user_item.dart
+++ lib/widgets/user_item.dart
@@ -1,6 +1,7 @@
 import 'package:OneTask/model/utente.dart';
 import 'package:flutter/material.dart';
 
+//rappresenta una classe di utilità di appoggio per rappresentare i singoli utenti
 class UserItem extends StatefulWidget {
   final Utente utente;  
   final onSelect;
@@ -23,18 +24,22 @@ class _UserItemState extends State<UserItem> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      //azione quando premi sulla riga
       onTap: () { 
         setState(() {
           isSelected = !isSelected; // Cambia lo stato di selezione quando viene premuto
+          //in base al valore del booleano si decide quale metodo invocare (aggiunta/cancellazione)
           if(isSelected){
             aggiunta = onSelect(utente);
           }else{
             onDeselect(utente);
           }
         });
-      },   //azione quando premi sulla riga
+      },  
+      //per arrotondare i bordi
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       tileColor: (isSelected & aggiunta) ? Colors.pink : Colors.blue.shade50,  //la riga si colora solo se puoi ancora aggiungere
+      //il testo di ogni singolo componente all'interno della lista di utenti - formato mat/cognome/nome
       title: Text(
         utente.matricola.toString() + " " + utente.cognome + " " + utente.nome,
       ),
