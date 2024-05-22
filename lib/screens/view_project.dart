b985db8abc34b0f32ab60f7d9284cfd2857abbdc
macron02 <m.mancino10@studenti.unisa.it>
2024-05-22 19:52:28 +0200

    aggiunta check boc

--- lib/screens/view_project.dart
+++ lib/screens/view_project.dart
@@ -3,7 +3,6 @@ import 'package:OneTask/model/task.dart';
 import 'package:OneTask/services/database_helper.dart';
 import 'package:flutter/material.dart';
 import '../widgets/appbar.dart';
-import '../widgets/task_section.dart';
 
 class ViewProject extends StatelessWidget {
   final String projectName;
@@ -58,110 +57,119 @@ class ViewProject extends StatelessWidget {
   }
 }
 
-class ProjectDetails extends StatelessWidget {
-  ProjectDetails({super.key, required this.projectName});
+class ProjectDetails extends StatefulWidget {
   final String projectName;
+
+  ProjectDetails({super.key, required this.projectName});
+
+  @override
+  _ProjectDetailsState createState() => _ProjectDetailsState();
+}
+
+class _ProjectDetailsState extends State<ProjectDetails> {
+  late Future<Progetto?> _futureProgetto;
+  late Future<List<Task>> _futureTasks;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureProgetto =
+        DatabaseHelper.instance.selectProgettoByNome(widget.projectName);
+    _futureTasks =
+        DatabaseHelper.instance.getTasksByProject(widget.projectName);
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
       child: FutureBuilder<Progetto?>(
-          future: DatabaseHelper.instance.selectProgettoByNome(projectName),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return const Text('Errore caricamento infoProgetto dal db');
-            } else {
-              //se non da problemi crea/restituisci la lista di teams
-              Progetto? progetto = snapshot.data;
-              if (progetto != null) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(progetto.nome,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text('Stato:${progetto.stato}',
-                        style: const TextStyle(
-                          fontSize: 15,
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text('Scadenza:${progetto.scadenza}',
-                        style: const TextStyle(
-                          fontSize: 15,
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text('Descrizione:${progetto.descrizione}',
-                        style: const TextStyle(
-                          fontSize: 15,
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text('Team:${progetto.team}',
-                        style: const TextStyle(
-                          fontSize: 15,
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text('I tuoi tasks',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    FutureBuilder<List<Task>>(
-                        future: DatabaseHelper.instance
-                            .getTasksByProject(projectName),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          } else if (snapshot.hasError) {
-                            return const Text(
-                                'Errore caricamento tasks del progetto dal db');
-                          } else {
-                            List<Task?> tasks = snapshot.data ?? [];
-                            return Column(
-                              children: tasks
-                                  .map(
-                                    (task) => Container(
-                                        margin:
-                                            const EdgeInsets.only(bottom: 8.0),
-                                        child: Row(children: [
-                                          _iconCheck(task),
-                                          Text(task!.attivita),
-                                        ])
-                                        //Container(color: Colors.blue, width: 30, height: 30)
-                                        ),
-                                  )
-                                  .toList(),
+        future: _futureProgetto,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return const Text('Errore caricamento infoProgetto dal db');
+          } else {
+            Progetto? progetto = snapshot.data;
+            if (progetto != null) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(progetto.nome,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const SizedBox(height: 20),
+                  Text('Stato: ${progetto.stato}',
+                      style: const TextStyle(fontSize: 15)),
+                  const SizedBox(height: 20),
+                  Text('Scadenza: ${progetto.scadenza}',
+                      style: const TextStyle(fontSize: 15)),
+                  const SizedBox(height: 20),
+                  Text('Descrizione: ${progetto.descrizione}',
+                      style: const TextStyle(fontSize: 15)),
+                  const SizedBox(height: 20),
+                  Text('Team: ${progetto.team}',
+                      style: const TextStyle(fontSize: 15)),
+                  const SizedBox(height: 20),
+                  const Text('I tuoi tasks',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  FutureBuilder<List<Task>>(
+                    future: _futureTasks,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return const Text(
+                            'Errore caricamento tasks del progetto dal db');
+                      } else {
+                        List<Task> tasks = snapshot.data ?? [];
+                        return Column(
+                          children: tasks.map((task) {
+                            return TaskItem(
+                              task: task,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  task.completato = value ?? false;
+                                  DatabaseHelper.instance.updateTask(task);
+                                });
+                              },
                             );
-                          }
-                        }),
-                  ],
-                );
-              } else {
-                return const Text('Progetto non trovato');
-              }
+                          }).toList(),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              );
+            } else {
+              return const Text('Progetto non trovato');
             }
-          }),
+          }
+        },
+      ),
     );
   }
+}
 
-  Icon _iconCheck(task) {
-    return Icon(
-        task.completato ? Icons.check_box : Icons.check_box_outline_blank);
+class TaskItem extends StatelessWidget {
+  final Task task;
+  final ValueChanged<bool?> onChanged;
+
+  TaskItem({required this.task, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      title: Text(task.attivita),
+      value: task.completato,
+      onChanged: onChanged,
+      controlAffinity: ListTileControlAffinity.leading,
+    );
   }
 }
